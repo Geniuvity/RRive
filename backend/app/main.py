85babfcd3d4589e709131a7dea76078bd3fd41d0
Yash-S-S <ysshivarkar05@gmail.com>
2026-04-06 18:52:19 +0530

    Changed sequence for production.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.routes import router
 from app.auth.auth_routes import auth_router
@@ -8,6 +8,15 @@ from app.config import APP_SECRET_KEY
 
 
 app = FastAPI()
+
+#Changing allow_origins from local host to domain for deployment
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://rrive.vercel.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Must be added BEFORE CORSMiddleware
 app.add_middleware(
@@ -19,18 +28,6 @@ app.add_middleware(
     # same_site="lax" is only for local deployment, since we have backend and frontend server on different domain, samesite="none"
     same_site="none",
     max_age=3600,
-)
-
-# Allows for both, local deployment, and server side deployment(like vercel).
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "https://rrive.vercel.app",
-        "http://localhost:5173",
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
 )
 
 app.include_router(auth_router)
